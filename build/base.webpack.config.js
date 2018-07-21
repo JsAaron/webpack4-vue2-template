9d07b2bf35c573d6e5cc6ddb28e42362aacff6c4
jsAaron <401545553@qq.com>
2018-07-21 20:13:43 +0800

    a

--- build/base.webpack.config.js
+++ build/base.webpack.config.js
@@ -7,18 +7,29 @@ const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
   entry: {
-    //公共代码
+    // 需要被提取为公共模块的群组
     commons: ["./src/jquery", "vue"],
     //逻辑代码
     app: './src/index.js'
   },
   output: {
+    //基于文件的md5生成Hash名称的script来防止缓存
     filename: '[name].bundle.js',
+    //输出文件，路径相对于本文件所在的位置
     path: config.dist,
+    // 设置publicPath这个属性会出现很多问题：
+    // 1.可以看成输出文件的另一种路径，差别路径是相对于生成的html文件；
+    // 2.也可以看成网站运行时的访问路径；
+    // 3.该属性的好处在于当你配置了图片CDN的地址，本地开发时引用本地的图片资源，上线打包时就将资源全部指向CDN了，
+    //   如果没有确定的发布地址不建议配置该属性，特别是在打包图片时，路径很容易出现混乱，
+    //   如果没有设置，则默认从站点根目录加载
+    // publicPath: '../static/js/',
     publicPath: config.publicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.less', '.css', '.scss']
+    extensions: ['.js', '.vue', '.less', '.css', '.scss'],
+    // 模块别名地址，方便后续直接引用别名，无须写长长的地址，注意如果后续不能识别该别名，需要先设置root
+    alias:{}
   },
   module: {
     rules: [{
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App'
 
+
+// console.log(process.env.NODE_ENV)
+
 new Vue({
   el: '#app',
   render: h => h(App)
